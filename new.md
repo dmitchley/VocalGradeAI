0e935d3380a4d14eeac73daa7754d6cfe17c72b6
Dylan Mitchley <[dylanmitchley1994@gmail.com]>
2023-07-30 22:54:38 +0200

    added open ai basic functionality

--- new.md
+++ new.md
@@ -1,9 +1,11 @@
-Great job! Just a few suggestions to improve your sentences:
+# Great job! Just a few suggestions to improve your sentences:
 
-1. "I am speaking for English now." 
+1. "I am speaking for English now."
+
 - Instead of using "for," you can say "in" or "using" English. So it would be: "I am speaking in English now" or "I am speaking using English now."
 
 2. "I am learning for English now."
+
 - Instead of using "for," you can use "to" or "in" to indicate the purpose of your learning. So it would be: "I am learning English now" or "I am learning in English now."
 
-Keep up the good work!
+## Keep up the good work!
--- trans.js
+++ trans.js
@@ -1,112 +1,432 @@
-API_TOKEN = "e1feba3ea0cd4255b423b8d60818367b";
+// // const fs = require("fs");
+// // const fetch = require("node-fetch");
+
+// // const { Configuration, OpenAIApi } = require("openai");
+
+// // const API_TOKEN = "e1feba3ea0cd4255b423b8d60818367b";
+// // const OPENAI_API_KEY = "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"; // Replace with your OpenAI API key
+// // const openai = new OpenAIApi(new Configuration({ apiKey: OPENAI_API_KEY }));
+
+// // async function getGrammarFeedback(text) {
+// //   try {
+// //     const prompt = `Review the following text for grammatical errors and suggest improvements:\n\n"${text}"`;
+
+// //     const response = await openai.complete({
+// //       model: "text-davinci-002",
+// //       prompt: prompt,
+// //       maxTokens: text.length + 100,
+// //     });
+
+// //     return response.data.choices[0].text.trim();
+// //   } catch (error) {
+// //     console.error(`Error with OpenAI API: ${error.message}`);
+// //     return null;
+// //   }
+// // }
+
+// // async function upload_file(api_token, path) {
+// //   const data = fs.readFileSync(path);
+// //   const url = "https://api.assemblyai.com/v2/upload";
+
+// //   try {
+// //     const response = await fetch(url, {
+// //       method: "POST",
+// //       body: data,
+// //       headers: {
+// //         "Content-Type": "application/octet-stream",
+// //         Authorization: api_token,
+// //       },
+// //     });
+
+// //     if (response.status === 200) {
+// //       const responseData = await response.json();
+// //       return responseData["upload_url"];
+// //     } else {
+// //       console.error(`Error: ${response.status} - ${response.statusText}`);
+// //       return null;
+// //     }
+// //   } catch (error) {
+// //     console.error(`Error: ${error}`);
+// //     return null;
+// //   }
+// // }
+
+// // async function transcribeAudio(api_token, audio_url) {
+// //   const headers = {
+// //     authorization: api_token,
+// //     "content-type": "application/json",
+// //   };
+
+// //   const response = await fetch("https://api.assemblyai.com/v2/transcript", {
+// //     method: "POST",
+// //     body: JSON.stringify({ audio_url }),
+// //     headers,
+// //   });
+
+// //   const responseData = await response.json();
+// //   const transcriptId = responseData.id;
+// //   const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId}`;
+
+// //   while (true) {
+// //     const pollingResponse = await fetch(pollingEndpoint, { headers });
+// //     const transcriptionResult = await pollingResponse.json();
+
+// //     if (transcriptionResult.status === "completed") {
+// //       return transcriptionResult;
+// //     } else if (transcriptionResult.status === "error") {
+// //       throw new Error(`Transcription failed: ${transcriptionResult.error}`);
+// //     } else {
+// //       await new Promise((resolve) => setTimeout(resolve, 3000));
+// //     }
+// //   }
+// // }
+
+// // async function main() {
+// //   const path = "./recorded.wav";
+// //   const uploadUrl = await upload_file(API_TOKEN, path);
+
+// //   if (!uploadUrl) {
+// //     console.error(new Error("Upload failed. Please try again."));
+// //     return;
+// //   }
+
+// //   const transcript = await transcribeAudio(API_TOKEN, uploadUrl);
+// //   console.log("Transcript:", transcript.text);
+
+// //   const feedback = await getGrammarFeedback(transcript.text);
+
+// //   if (feedback) {
+// //     console.log("OpenAI Feedback:", feedback);
+// //     fs.writeFile("./feedback.txt", feedback, (err) => {
+// //       if (err) {
+// //         console.error(err);
+// //       }
+// //       console.log("Feedback written to feedback.txt");
+// //     });
+// //   } else {
+// //     console.error("Failed to get feedback from OpenAI.");
+// //   }
+
+// //   fs.writeFile("./test.txt", transcript.text, (err) => {
+// //     if (err) {
+// //       console.error(err);
+// //     }
+// //     console.log("Transcript written to test.txt");
+// //   });
+// // }
+
+// // main();
+
+// const API_TOKEN = "e1feba3ea0cd4255b423b8d60818367b";
+// const OPENAI_API_KEY = "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";
+// const fs = require("fs");
+// const fetch = require("node-fetch");
+
+// async function main() {
+//   console.log("Welcome to AssemblyAI!");
+
+//   async function upload_file(api_token, path) {
+//     console.log(`Uploading file: ${path}`);
+//     const data = fs.readFileSync(path);
+//     const url = "https://api.assemblyai.com/v2/upload";
+
+//     const response = await fetch(url, {
+//       method: "POST",
+//       body: data,
+//       headers: {
+//         "Content-Type": "application/octet-stream",
+//         Authorization: api_token,
+//       },
+//     });
+
+//     if (response.status === 200) {
+//       const responseData = await response.json();
+//       return responseData["upload_url"];
+//     } else {
+//       console.error(`Error: ${response.status} - ${response.statusText}`);
+//       return null;
+//     }
+//   }
+
+//   async function transcribeAudio(api_token, audio_url) {
+//     const headers = {
+//       authorization: api_token,
+//       "content-type": "application/json",
+//     };
+//     const response = await fetch("https://api.assemblyai.com/v2/transcript", {
+//       method: "POST",
+//       body: JSON.stringify({ audio_url }),
+//       headers,
+//     });
+//     const responseData = await response.json();
+//     const transcriptId = responseData.id;
+//     const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId}`;
+
+//     while (true) {
+//       const pollingResponse = await fetch(pollingEndpoint, { headers });
+//       const transcriptionResult = await pollingResponse.json();
+
+//       if (transcriptionResult.status === "completed") {
+//         return transcriptionResult;
+//       } else if (transcriptionResult.status === "error") {
+//         throw new Error(`Transcription failed: ${transcriptionResult.error}`);
+//       } else {
+//         await new Promise((resolve) => setTimeout(resolve, 3000));
+//       }
+//     }
+//   }
+
+//   async function getOpenAIFeedback(text) {
+//     //const url = "https://api.openai.com/v1/engines/davinci/completions";
+//     const url = "https://api.openai.com/v1/chat/completions";
+
+//     const headers = {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${OPENAI_API_KEY}`,
+//     };
+//     const response = await fetch(url, {
+//       method: "POST",
+//       headers: headers,
+//       body: JSON.stringify({
+//         prompt: `Given the following text, please provide corrections for grammar, clarity, and style: "${text}"`,
+//         max_tokens: 50,
+//       }),
+//     });
+
+//     const responseData = await response.json();
+//     return (
+//       responseData.choices &&
+//       responseData.choices[0] &&
+//       responseData.choices[0].text.trim()
+//     );
+//   }
+
+//   const path = "./recorded.wav";
+//   const uploadUrl = await upload_file(API_TOKEN, path);
+
+//   if (!uploadUrl) {
+//     console.error(new Error("Upload failed. Please try again."));
+//     return;
+//   }
+
+//   const transcript = await transcribeAudio(API_TOKEN, uploadUrl);
+//   console.log("Original Transcript:", transcript.text);
+
+//   const improvedText = await getOpenAIFeedback(transcript.text);
+//   console.log("Improved Text:", improvedText);
+
+//   fs.writeFile("./test.txt", improvedText, (err) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     console.log("Improved transcript written to test.txt");
+//   });
+// }
+
+// main();
+
+// const API_TOKEN = "e1feba3ea0cd4255b423b8d60818367b";
+// const OPENAI_API_KEY = "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";
+// const fs = require("fs");
+// const fetch = require("node-fetch");
+
+// async function main() {
+//   console.log("Welcome to AssemblyAI!");
+
+//   async function upload_file(api_token, path) {
+//     console.log(`Uploading file: ${path}`);
+//     const data = fs.readFileSync(path);
+//     const url = "https://api.assemblyai.com/v2/upload";
+
+//     const response = await fetch(url, {
+//       method: "POST",
+//       body: data,
+//       headers: {
+//         "Content-Type": "application/octet-stream",
+//         Authorization: api_token,
+//       },
+//     });
+
+//     if (response.status === 200) {
+//       const responseData = await response.json();
+//       return responseData["upload_url"];
+//     } else {
+//       console.error(`Error: ${response.status} - ${response.statusText}`);
+//       return null;
+//     }
+//   }
+
+//   async function transcribeAudio(api_token, audio_url) {
+//     const headers = {
+//       authorization: api_token,
+//       "content-type": "application/json",
+//     };
+//     const response = await fetch("https://api.assemblyai.com/v2/transcript", {
+//       method: "POST",
+//       body: JSON.stringify({ audio_url }),
+//       headers,
+//     });
+//     const responseData = await response.json();
+//     const transcriptId = responseData.id;
+//     const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId}`;
+
+//     while (true) {
+//       const pollingResponse = await fetch(pollingEndpoint, { headers });
+//       const transcriptionResult = await pollingResponse.json();
+
+//       if (transcriptionResult.status === "completed") {
+//         return transcriptionResult;
+//       } else if (transcriptionResult.status === "error") {
+//         throw new Error(`Transcription failed: ${transcriptionResult.error}`);
+//       } else {
+//         await new Promise((resolve) => setTimeout(resolve, 3000));
+//       }
+//     }
+//   }
+
+// async function getOpenAIFeedback(text) {
+//   const url = " https://api.openai.com/v1/completions";
+
+//   const headers = {
+//     "Content-Type": "application/json",
+//     Authorization: `Bearer ${OPENAI_API_KEY}`,
+//   };
+//   const response = await fetch(url, {
+//     method: "POST",
+//     headers: headers,
+//     body: JSON.stringify({
+//       prompt: `Given the following text, please provide corrections for grammar, clarity, and style: "${text}"`,
+//       max_tokens: 150, // I increased the tokens limit as 50 might be too restrictive.
+//     }),
+//   });
+
+//   const responseData = await response.json();
+
+//   if (responseData.error) {
+//     console.error(`OpenAI API error: ${responseData.error.message}`);
+//     return null;
+//   }
+
+//   return (
+//     responseData.choices &&
+//     responseData.choices[0] &&
+//     responseData.choices[0].text.trim()
+//   );
+// }
+
 const fs = require("fs");
-// Upload a file and create a transcript using the AssemblyAI API
+const fetch = require("node-fetch");
+const { Configuration, OpenAIApi } = require("openai");
+
+const API_TOKEN = "e1feba3ea0cd4255b423b8d60818367b";
+const OPENAI_API_KEY = "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";
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+async function getOpenAIFeedback(text) {
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `You will be provided with statements, and your task is give feedback line by line to the student on how they can improve their sentence.\nUSER\n${text}`,
+        },
+      ],
+      temperature: 0,
+      max_tokens: 256,
+    });
+
+    return (
+      response.data.choices && response.data.choices[0].message.content.trim()
+    );
+  } catch (error) {
+    console.error(`OpenAI API error: ${error.message}`);
+    return null;
+  }
+}
+
+async function upload_file(api_token, path) {
+  console.log(`Uploading file: ${path}`);
+  const data = fs.readFileSync(path);
+  const url = "https://api.assemblyai.com/v2/upload";
+
+  const response = await fetch(url, {
+    method: "POST",
+    body: data,
+    headers: {
+      "Content-Type": "application/octet-stream",
+      Authorization: api_token,
+    },
+  });
+
+  if (response.status === 200) {
+    const responseData = await response.json();
+    return responseData["upload_url"];
+  } else {
+    console.error(`Error: ${response.status} - ${response.statusText}`);
+    return null;
+  }
+}
+
+async function transcribeAudio(api_token, audio_url) {
+  const headers = {
+    authorization: api_token,
+    "content-type": "application/json",
+  };
+  const response = await fetch("https://api.assemblyai.com/v2/transcript", {
+    method: "POST",
+    body: JSON.stringify({ audio_url }),
+    headers,
+  });
+  const responseData = await response.json();
+  const transcriptId = responseData.id;
+  const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId}`;
+
+  while (true) {
+    const pollingResponse = await fetch(pollingEndpoint, { headers });
+    const transcriptionResult = await pollingResponse.json();
+
+    if (transcriptionResult.status === "completed") {
+      return transcriptionResult;
+    } else if (transcriptionResult.status === "error") {
+      throw new Error(`Transcription failed: ${transcriptionResult.error}`);
+    } else {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+  }
+}
+
 async function main() {
   console.log("Welcome to AssemblyAI!");
 
-  async function upload_file(api_token, path) {
-    console.log(`Uploading file: ${path}`);
-
-    // Read the file data
-    const data = fs.readFileSync(path);
-    const url = "https://api.assemblyai.com/v2/upload";
-
-    try {
-      // Send a POST request to the API to upload the file, passing in the headers and the file data
-      const response = await fetch(url, {
-        method: "POST",
-        body: data,
-        headers: {
-          "Content-Type": "application/octet-stream",
-          Authorization: api_token,
-        },
-      });
-
-      // If the response is successful, return the upload URL
-      if (response.status === 200) {
-        const responseData = await response.json();
-        return responseData["upload_url"];
-      } else {
-        console.error(`Error: ${response.status} - ${response.statusText}`);
-        return null;
-      }
-    } catch (error) {
-      console.error(`Error: ${error}`);
-      return null;
-    }
-  }
-
-  async function transcribeAudio(api_token, audio_url) {
-    console.log("Transcribing audio... This might take a moment.");
-
-    // Set the headers for the request, including the API token and content type
-    const headers = {
-      authorization: api_token,
-      "content-type": "application/json",
-    };
-
-    // Send a POST request to the transcription API with the audio URL in the request body
-    const response = await fetch("https://api.assemblyai.com/v2/transcript", {
-      method: "POST",
-      body: JSON.stringify({ audio_url }),
-      headers,
-    });
-
-    // Retrieve the ID of the transcript from the response data
-    const responseData = await response.json();
-    const transcriptId = responseData.id;
-
-    // Construct the polling endpoint URL using the transcript ID
-    const pollingEndpoint = `https://api.assemblyai.com/v2/transcript/${transcriptId}`;
-
-    // Poll the transcription API until the transcript is ready
-    while (true) {
-      // Send a GET request to the polling endpoint to retrieve the status of the transcript
-      const pollingResponse = await fetch(pollingEndpoint, { headers });
-      const transcriptionResult = await pollingResponse.json();
-
-      // If the transcription is complete, return the transcript object
-      if (transcriptionResult.status === "completed") {
-        return transcriptionResult;
-      }
-      // If the transcription has failed, throw an error with the error message
-      else if (transcriptionResult.status === "error") {
-        throw new Error(`Transcription failed: ${transcriptionResult.error}`);
-      }
-      // If the transcription is still in progress, wait for a few seconds before polling again
-      else {
-        await new Promise((resolve) => setTimeout(resolve, 3000));
-      }
-    }
-  }
-
-  // -----------------------------------------------------------------------------
-  // Update the file path here, pointing to a local audio or video file.
-  // If you don't have one, download a sample file: https://storage.googleapis.com/aai-web-samples/espn-bears.m4a
-  // You may also remove the upload step and update the 'audio_url' parameter in the
-  // 'transcribeAudio' function to point to a remote audio or video file.
-  // -----------------------------------------------------------------------------
   const path = "./recorded.wav";
   const uploadUrl = await upload_file(API_TOKEN, path);
 
-  // If the upload fails, log an error and return
   if (!uploadUrl) {
     console.error(new Error("Upload failed. Please try again."));
     return;
   }
 
-  // Transcribe the audio file using the upload URL
   const transcript = await transcribeAudio(API_TOKEN, uploadUrl);
+  console.log("Original Transcript:", transcript.text);
 
-  // Print the completed transcript object
-  console.log("Transcript:", transcript.text);
+  const improvedText = await getOpenAIFeedback(transcript.text);
+  console.log("Improved Text:", improvedText);
 
-  fs.writeFile("./test.txt", transcript.text, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    // file written successfully
-  });
+  if (improvedText) {
+    fs.writeFile("./test.txt", improvedText, (err) => {
+      if (err) {
+        console.error(err);
+      }
+      console.log("Improved transcript written to test.txt");
+    });
+  } else {
+    console.error("Failed to get an improved text.");
+  }
 }
 
 main();
